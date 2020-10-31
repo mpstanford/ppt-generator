05546faf1bbee178ea4a61f4598036ff6993446e
Madison Stanford <madison.stanford@gmail.com>
2020-10-30 20:54:13 -0700

    Implement Hello World with React

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,18 @@
-import react from 'react';
-import reactDom from 'react-dom';
+import React from 'react';
+import ReactDom from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello World React</h1>
+    );
+  }
+}
+
+
+
+ReactDom.render(<App />, document.getElementById('app'));
